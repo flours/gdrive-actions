c4701e6a61e621a3c69ad1f663a401d913e41a4c
flours <recf6978@yahoo.co.jp>
2023-01-30 16:54:49 +0900

    commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,6 @@
 
 mkdir -p /tmp
 echo $CLIENT_SECRET > /tmp/client_secrets.json
-export GOOGLE_APPLICATION_CREDENTIALS=/tmp/client_secrets.json
+export GOOGLE_APPLICATION_CREDENTIALS="/tmp/client_secrets.json"
 
 /downloader $DOWNLOAD_FILE_ID $DOWNLOAD_PATH
